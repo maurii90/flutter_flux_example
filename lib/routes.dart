0840598aac11a3fe0c8041136b145611cdfeb787
Maurizio Silipo <masilipo@gmail.com>
2018-06-09 17:07:45 +0200

    Introduce choose service page

---
+++ lib/routes.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+// import "package:youtrack_timer/pages/login_page.dart";
+// import "package:youtrack_timer/pages/project_list_page.dart";
+// import "package:youtrack_timer/pages/choose_ticket_page.dart";
+import "package:youtrackr/pages/choose_service_page.dart";
+
+final routes = <String, WidgetBuilder> {
+  ChooseServicePage.tag: (context) => new ChooseServicePage(),
+  // LoginPage.tag: (context) => new LoginPage(),
+  // ProjectListPage.tag: (context) => new ProjectListPage(),
+  // ChooseTicketPage.tag: (context) => new ChooseTicketPage()
+};
